58a6fb127ad0ea7731f14cd347106974c723d33a
GeeKaven <i@tawawa.moe>
2022-02-24 00:43:22 +0800

    feat: support cors

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 import AutoLoad from 'fastify-autoload'
+import Cors from 'fastify-cors'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default async function (fastify, opts) {
-    fastify.register(AutoLoad, {
-        dir: join(__dirname, 'routes'),
-        dirNameRoutePrefix: false,
-        options: Object.assign({}, opts)
-      })
+  fastify.register(Cors, {
+    origin: true
+  })
+
+  fastify.register(AutoLoad, {
+    dir: join(__dirname, 'routes'),
+    dirNameRoutePrefix: false,
+    options: Object.assign({}, opts)
+  })
 }
