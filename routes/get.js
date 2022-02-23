2ab32a782848dc2af46c87fd8e413f2ad77fdc52
GeeKaven <i@tawawa.moe>
2022-02-23 22:50:00 +0800

    feat: add get model and textures

--- routes/get.js
+++ routes/get.js
@@ -1,3 +1,6 @@
+import { readFile } from 'fs/promises'
+import { getModelList, modelIdToName, getModelTexturesName } from '../tools/utils.js'
+
 export default async function get(fastify, opts) {
 
 	const routeOpts = {
@@ -14,8 +17,70 @@ export default async function get(fastify, opts) {
 	fastify.get('/get', routeOpts, handler)
 
 	async function handler(req, reply) {
-		reply.send({
-			message: req.query
-		})
+		const { id } = req.query;
+		let modelId = id.split('-')[0];
+		let modelTexturesId =  id.split('-')[1] ? id.split('-')[1] : 0;
+		
+		console.log(modelId, modelTexturesId);
+		let json = {}
+		const modelList = await getModelList();
+		let modelName = modelIdToName(modelList, modelId);
+		console.log(modelName);
+
+		if (Array.isArray(modelName)) {
+			modelName = modelTexturesId > 0 && modelTexturesId <= modelName.length ? modelName[modelTexturesId - 1] : modelName[0];
+			json = JSON.parse(
+				await readFile(
+					new URL(`../model/${modelName}/index.json`, import.meta.url),
+				)
+			)
+		} else {
+			json = JSON.parse(
+				await readFile(
+					new URL(`../model/${modelName}/index.json`, import.meta.url),
+				)
+			)
+			if (modelTexturesId > 0) {
+				const modelTexturesName = await getModelTexturesName(modelName, modelTexturesId);
+				if (modelTexturesName) {
+					json['textures'] = Array.isArray(modelTexturesName) ? modelTexturesName : [modelTexturesName];
+				}
+			}
+		}
+
+		json['textures'] = json['textures'].map(texture => `../model/${modelName}/${texture}`)
+
+		json['model'] = `../model/${modelName}/${json['model']}`
+
+		if (json['pose']) {
+			json['pose'] = `../model/${modelName}/${json['pose']}`
+		}
+
+		if (json['physics']) {
+			json['physics'] = `../model/${modelName}/${json['physics']}`
+		}
+
+		if (json['motions']) {
+			for (const k1 in json['motions']) {
+				for (const k2 in json['motions'][k1]) {
+					for (const k3 in json['motions'][k1][k2]) {
+						if (k3 == 'file' || k3 == 'sound') {
+							json['motions'][k1][k2][k3] = `../model/${modelName}/${json['motions'][k1][k2][k3]}`
+						}
+					}
+				}
+			}
+		}
+
+		if (json['expressions']) {
+			for (const k1 in json['expressions']) {
+				for (const k2 in json['expressions'][k1]) {
+					if (k2 == 'file') {
+						json['expressions'][k1][k2] = `../model/${modelName}/${json['expressions'][k1][k2]}`
+					}
+				}
+			}
+		}
+		reply.send(json);
 	}
 }
---
+++ tools/utils.js
@@ -0,0 +1,57 @@
+import { readFile, readdir, writeFile } from 'fs/promises'
+
+async function getModelList() {
+  const modelList = JSON.parse(
+    await readFile(
+      new URL('../model_list.json', import.meta.url),
+    )
+  )
+  return modelList
+}
+
+function modelIdToName(modelList, id) {
+  return modelList['models'][id - 1]
+}
+
+/* 获取材质名称 */
+async function getModelTexturesName(modelName, id) {
+  const list = await getModelTexturesList(modelName)
+  return list ? list[id - 1] : null
+}
+
+/* 获取列表缓存 */
+async function getModelTexturesList(modelName) {
+  let texturesCache
+  try {
+    texturesCache = JSON.parse(
+      await readFile(
+        new URL(`../model/${modelName}/textures.cache`, import.meta.url),
+      )
+    )
+    
+  } catch (error) {
+    texturesCache = await getTextures(modelName)
+    if (texturesCache) {
+      await writeFile(
+        new URL(`../model/${modelName}/textures.cache`, import.meta.url),
+        JSON.stringify(texturesCache)
+      )
+    }
+  }
+
+  return texturesCache ? texturesCache : []
+}
+
+/* 获取材质列表 */
+async function getTextures(modelName) {
+  console.log(import.meta.url)
+  try {
+    const files = await readdir(new URL(`../model/${modelName}/textures`, import.meta.url))
+    return files.map(file => `textures/${file}`)
+  } catch (error) {
+    console.log(`no dir ../model/${modelName}/textures`)
+  }
+  return []
+}
+
+export { getModelList, modelIdToName, getModelTexturesName }
